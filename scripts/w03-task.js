470a2fdb505403b968a716a0f44c0e603c0e7d85
whdiaz <whdiaztorres@gmail.com>
2024-01-29 14:10:05 -0300

    fix the assignament w03

--- scripts/w03-task.js
+++ scripts/w03-task.js
@@ -1,85 +1,79 @@
 /* LESSON 3 - Programming Tasks */
 
 /* FUNCTIONS */
-
-/* Function Definition - Addition Feature */
-function add(number1, number2) {
+/* Function Definition - Add Numbers */
+function add(number1, number2){
   return number1 + number2;
 }
-
-/* Function Declaration - Add Numbers */
-function addNumbers() {
-  let addNumber1 = Number(document.querySelector('#add1').value);
-  let addNumber2 = Number(document.querySelector('#add2').value);
-  document.querySelector('#sum').value = add(addNumber1, addNumber2);
+function addNumbers(){
+  let number1 = Number(document.getElementById('add1').value);
+  let number2 = Number(document.getElementById('add2').value);
+  document.getElementById('sum').value = add(number1, number2);
 }
-
-/* Function Expression - Subtraction Feature */
-const subtract = function(number1, number2) {
-  return number1 - number2;
-};
+document.getElementById("addNumbers").addEventListener("click", addNumbers);
 
 /* Function Expression - Subtract Numbers */
-const subtractNumbers = function() {
-  let subtractNumber1 = Number(document.querySelector('#subtract1').value);
-  let subtractNumber2 = Number(document.querySelector('#subtract2').value);
-  document.querySelector('#difference').value = subtract(subtractNumber1, subtractNumber2);
+let subtract = function(subtract1, subtract2){
+  return subtract1 - subtract2;
 };
-
-/* Arrow Function - Multiplication Feature */
-const multiply = (number1, number2) => number1 * number2;
+let subtractNumbers = function(){
+  let subtract1 = Number(document.getElementById('subtract1').value);
+  let subtract2 = Number(document.getElementById('subtract2').value);
+  document.getElementById('difference').value = subtract(subtract1, subtract2);
+};
+document.getElementById("subtractNumbers").addEventListener("click", subtractNumbers);
 
 /* Arrow Function - Multiply Numbers */
-const multiplyNumbers = () => {
-  let factor1 = Number(document.querySelector('#factor1').value);
-  let factor2 = Number(document.querySelector('#factor2').value);
-  document.querySelector('#product').value = multiply(factor1, factor2);
+let multiply = (factor1, factor2) => factor1 * factor2;
+let multiplyNumbers = () => {
+  let factor1 = Number(document.getElementById('factor1').value);
+  let factor2 = Number(document.getElementById('factor2').value);
+  document.getElementById('product').value = multiply(factor1, factor2);
 };
-
-/* Function Declaration - Division Feature */
-function divide(dividend, divisor) {
-  return dividend / divisor;
-}
+document.getElementById("multiplyNumbers").addEventListener("click", multiplyNumbers);
 
 /* Open Function Use - Divide Numbers */
-function divideNumbers() {
-  let dividend = Number(document.querySelector('#dividend').value);
-  let divisor = Number(document.querySelector('#divisor').value);
-  document.querySelector('#quotient').value = divide(dividend, divisor);
-}
+let divide = (dividend, divisor) => dividend / divisor;
+let divideNumbers = function(){
+  let dividend = Number(document.getElementById('dividend').value);
+  let divisor = Number(document.getElementById('divisor').value);
+  document.getElementById('quotient').value = divide(dividend, divisor);
+};
+document.getElementById("divideNumbers").addEventListener("click", divideNumbers);
 
-/* Selection Structures */
-document.getElementById("getTotal").addEventListener("click", function() {
-  // Declare and instantiate a variable for the subtotal
-  let subtotal = parseFloat(document.getElementById("subtotal").value) || 0;
-
-  // Check if the membership checkbox is checked for a discount
-  let isMember = document.getElementById("member").checked;
-
-  // Apply discount if the user is a member
-  let discountedTotal = isMember ? subtotal * 0.8 : subtotal;
-
-  // Output the total with two decimals using template string
-  document.getElementById("total").textContent = `$${discountedTotal.toFixed(2)}`;
-});
+/* Decision Structure */
+let getTotalDue = ()=>{
+  let subtotal = Number(document.getElementById("subtotal").value);
+  let membershipInput = document.getElementById("member").checked;
+  if(membershipInput){
+      subtotal = subtotal * 0.8;
+  }
+  subtotal = subtotal.toFixed(2);
+  document.getElementById("total").innerHTML = `$ ${subtotal}`;
+};
+document.getElementById("getTotal").addEventListener("click", getTotalDue);
 
 /* ARRAY METHODS - Functional Programming */
-let numbersArray = Array.from({ length: 13 }, (_, index) => index + 1);
+/* Output Source Array */
+let numbersArray = [];
+for(var i=1; i<=13;i++){
+  numbersArray.push(i);
+}
+document.getElementById('array').innerHTML = numbersArray;
 
-/* Assign the value of the array variable to the HTML element with an ID of array */
-document.getElementById("array").textContent = numbersArray.join(", ");
+/* Output Odds Only Array */
+document.getElementById('odds').innerHTML = numbersArray.filter((number) => number % 2);
 
-/* Use the filter() array method to find all odd numbers and assign the result to the HTML element with an ID of odds */
-document.getElementById("odds").textContent = numbersArray.filter(number => number % 2 !== 0).join(", ");
+/* Output Evens Only Array */
+document.getElementById('evens').innerHTML = numbersArray.filter((number) => !(number % 2));
 
-/* Use the filter() array method to find all even numbers and assign the result to the HTML element with an ID of evens */
-document.getElementById("evens").textContent = numbersArray.filter(number => number % 2 === 0).join(", ");
+/* Output Sum of Org. Array */
+document.getElementById('sumOfArray').innerHTML = numbersArray.reduce( (total, number) => total + number );
 
-/* Use the reduce() array method to sum the array variable elements and assign the result to the HTML element with an ID of sumOfArray */
-document.getElementById("sumOfArray").textContent = numbersArray.reduce((sum, number) => sum + number, 0);
+/* Output Multiplied by 2 Array */
+document.getElementById('multiplied').innerHTML = numbersArray.map((number) => number * 2);
 
-/* Use the map() array method to multiply each element by 2 and assign the result to the HTML element with an ID of multiplied */
-document.getElementById("multiplied").textContent = numbersArray.map(number => number * 2).join(", ");
-
-/* Use the map() and reduce() array methods to sum the array elements after multiplying each element by two. Assign the result to the HTML element with an ID of sumOfMultiplied */
-document.getElementById("sumOfMultiplied").textContent = numbersArray.map(number => number * 2).reduce((sum, number) => sum + number, 0);
+/* Output Sum of Multiplied by 2 Array */
+let multiplyArray = numbersArray.map((number) => number * 2);
+let totalMultiply = multiplyArray.reduce((total, number)=>total+number);
+document.getElementById('sumOfMultiplied').innerHTML = totalMultiply;
